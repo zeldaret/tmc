c8ae131cd99297194dc57b159fff9207c9b98e7a
chandler <chandlerm2016@yahoo.com>
2020-06-12 01:52:58 -0400

    style

--- src/bell.c
+++ src/bell.c
@@ -1,26 +1,28 @@
-#include "global.h"
 #include "entity.h"
+#include "global.h"
 
 extern void sub_080042B8();
-extern void (*gUnk_08123384[])(Entity *);
+extern void (*gUnk_08123384[])(Entity*);
 
-extern void UpdateSpriteOrderAndFlip(Entity *);
+extern void UpdateSpriteOrderAndFlip(Entity*);
 extern void LoadAnimation();
 
-
-void sub_08097D78(Entity* this){
-    gUnk_08123384[this->action](this);
+void sub_08097D78(Entity *ent) 
+{
+    gUnk_08123384[ent->action](ent);
 }
 
-void sub_08097D90(Entity* this){
-    this->action = 1;
-    this->spriteSettings.b.ss0 = 1;
-    this->collisionLayer = 1;
-    this->ticks.b0 = 0;
-    UpdateSpriteOrderAndFlip(this);
-    LoadAnimation(this, 0);
+void sub_08097D90(Entity *ent)
+{
+  ent->action = 1;
+  ent->spriteSettings.b.ss0 = 1;
+  ent->collisionLayer = 1;
+  ent->ticks.b0 = 0;
+  UpdateSpriteOrderAndFlip(ent);
+  LoadAnimation(ent, 0);
 }
 
-void sub_08097DCC(Entity* this){
-    sub_080042B8(this);
+void sub_08097DCC(Entity *ent)
+{
+    sub_080042B8(ent);
 }
