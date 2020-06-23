344de9129d5bd8fda75e443f801cb56a2be55dc2
theo3 <arisstephenson2@gmail.com>
2020-06-23 15:15:27 -0700

    defined areaPropertyLists.s

---
+++ src/manager2E.c
@@ -0,0 +1,15 @@
+#include "global.h"
+#include "entity.h"
+
+extern void (*const gUnk_08108D3C[])(Entity*);
+extern void (*const gUnk_08108D44[])(Entity*);
+
+void sub_0805D250(Entity *this)
+{
+  gUnk_08108D3C[this->entityType.form](this);
+}
+
+void sub_0805D268(Entity *this)
+{
+  gUnk_08108D44[this->action](this);
+}
