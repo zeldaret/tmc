0316bb142635ca21cecbece596ee675e0ae4492e
Behemoth <Luisscheurenbrand@gmail.com>
2020-09-19 01:37:43 +0200

    decompile WallMaster

--- data/data_080CC6FC.s
+++ data/data_080CC6FC.s
@@ -3,27 +3,6 @@
 
 	.section .rodata
 
-gUnk_080CD07C:: @ 080CD07C
-	.incbin "baserom.gba", 0x0CD07C, 0x0000018
-
-gUnk_080CD094:: @ 080CD094
-	.incbin "baserom.gba", 0x0CD094, 0x000001C
-
-gUnk_080CD0B0:: @ 080CD0B0
-	.incbin "baserom.gba", 0x0CD0B0, 0x0000008
-
-gUnk_080CD0B8:: @ 080CD0B8
-	.incbin "baserom.gba", 0x0CD0B8, 0x000000D
-
-gUnk_080CD0C5:: @ 080CD0C5
-	.incbin "baserom.gba", 0x0CD0C5, 0x0000015
-
-gUnk_080CD0DA:: @ 080CD0DA
-	.incbin "baserom.gba", 0x0CD0DA, 0x0000006
-
-gUnk_080CD0E0:: @ 080CD0E0
-	.incbin "baserom.gba", 0x0CD0E0, 0x0000010
-
 gUnk_080CD0F0:: @ 080CD0F0
 	.incbin "baserom.gba", 0x0CD0F0, 0x0000018
 
