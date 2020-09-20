0703d4ad8bf6f630998b5370a6c18399c7ad09fd
Behemoth <Luisscheurenbrand@gmail.com>
2020-09-20 17:50:15 +0200

    decompile SpikedBeetle

--- data/data_080CC6FC.s
+++ data/data_080CC6FC.s
@@ -3,51 +3,6 @@
 
 	.section .rodata
 
-gUnk_080CD2D8:: @ 080CD2D8
-	.incbin "baserom.gba", 0x0CD2D8, 0x0000018
-
-gUnk_080CD2F0:: @ 080CD2F0
-	.incbin "baserom.gba", 0x0CD2F0, 0x0000024
-
-gUnk_080CD314:: @ 080CD314
-	.incbin "baserom.gba", 0x0CD314, 0x0000004
-
-gUnk_080CD318:: @ 080CD318
-	.incbin "baserom.gba", 0x0CD318, 0x0000004
-
-gUnk_080CD31C:: @ 080CD31C
-	.incbin "baserom.gba", 0x0CD31C, 0x0000011
-
-gUnk_080CD32D:: @ 080CD32D
-	.incbin "baserom.gba", 0x0CD32D, 0x0000011
-
-gUnk_080CD33E:: @ 080CD33E
-	.incbin "baserom.gba", 0x0CD33E, 0x0000011
-
-gUnk_080CD34F:: @ 080CD34F
-	.incbin "baserom.gba", 0x0CD34F, 0x0000011
-
-gUnk_080CD360:: @ 080CD360
-	.incbin "baserom.gba", 0x0CD360, 0x0000008
-
-gUnk_080CD368:: @ 080CD368
-	.incbin "baserom.gba", 0x0CD368, 0x0000008
-
-gUnk_080CD370:: @ 080CD370
-	.incbin "baserom.gba", 0x0CD370, 0x0000008
-
-gUnk_080CD378:: @ 080CD378
-	.incbin "baserom.gba", 0x0CD378, 0x0000008
-
-gUnk_080CD380:: @ 080CD380
-	.incbin "baserom.gba", 0x0CD380, 0x0000011
-
-gUnk_080CD391:: @ 080CD391
-	.incbin "baserom.gba", 0x0CD391, 0x0000007
-
-gUnk_080CD398:: @ 080CD398
-	.incbin "baserom.gba", 0x0CD398, 0x000002C
-
 gUnk_080CD3C4:: @ 080CD3C4
 	.incbin "baserom.gba", 0x0CD3C4, 0x0000010
 
