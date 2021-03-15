e541025bb7c5bc4d4abeccc10bf0aef8f2662024
gamestabled <chandler2016@yahoo.com>
2021-03-15 02:40:37 -0400

    MinishSizedArchway OK

---
+++ src/object/minishSizedArchway.c
@@ -0,0 +1,18 @@
+#include "global.h"
+#include "entity.h"
+#include "sprite.h"
+
+extern u32 CheckIsDungeon();
+
+void MinishSizedArchway(Entity* this) {
+    if (this->action == 0) {
+        this->action = 1;
+        this->frameIndex = this->type;
+        this->collisionLayer = 2;
+        UpdateSpriteForCollisionLayer(this);
+        if (CheckIsDungeon()) {
+            this->spritePriority.b0 = (this->spritePriority.b0 & 0xf8) | 1;
+        }
+    }
+    return;
+}
