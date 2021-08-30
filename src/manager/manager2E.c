14323b92becbd6a3e24c09785c7085e4a2c4b31b
octorock <79596758+octorock@users.noreply.github.com>
2021-08-30 17:55:34 +0200

    Decompile manager2E

--- src/manager/manager2E.c
+++ src/manager/manager2E.c
@@ -1,27 +1,112 @@
 #include "global.h"
 #include "entity.h"
+#include "manager.h"
+#include "flags.h"
+#include "functions.h"
+#include "audio.h"
 
-extern void (*const gUnk_08108D3C[])(Entity*);
-extern void (*const gUnk_08108D44[])(Entity*);
+typedef struct {
+    Manager manager;
+    u8 field_0x20[0x18];
+    s16 field_0x38;
+    u8 field_0x3a;
+    u8 field_0x3b;
+    u16 field_0x3c;
+    u16 field_0x3e;
+} Manager2E;
 
-void Manager2E_Main(Entity* this) {
-    gUnk_08108D3C[this->type](this);
+extern void (*const gUnk_08108D3C[])(Manager2E*);
+extern void (*const gUnk_08108D44[])(Manager2E*);
+
+void sub_0805D2F4(Manager2E*);
+extern void sub_0805E4E0(void*, u32);
+
+extern void (*const gUnk_08108D50[])(Manager2E*);
+
+void Manager2E_Main(Manager2E* this) {
+    gUnk_08108D3C[this->manager.unk_0a](this);
 }
 
-void sub_0805D268(Entity* this) {
-    gUnk_08108D44[this->action](this);
+void sub_0805D268(Manager2E* this) {
+    gUnk_08108D44[this->manager.action](this);
 }
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D280.inc", void sub_0805D280())
+void sub_0805D280(Manager2E* this) {
+    if (CheckFlags(this->field_0x3c) != 0) {
+        DeleteThisEntity();
+    }
+    if (CheckFlags(this->field_0x3e) != 0) {
+        SetFlag(this->field_0x3c);
+        DeleteThisEntity();
+    }
+    this->manager.action = 1;
+    if (this->manager.unk_0e == 0) {
+        this->manager.unk_0e = 0x1e;
+    }
+    sub_0805E3A0(this, 6);
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D2C0.inc", void sub_0805D2C0())
+void sub_0805D2C0(Manager2E* this) {
+    if ((CheckFlags(this->field_0x3e) != 0) && (gUnk_03003DC0.unk_0xc < 2)) {
+        this->manager.action = 2;
+        if (this->manager.unk_0e == 1) {
+            sub_0805D2F4(this);
+        }
+        sub_0805E4E0(this, this->manager.unk_0e);
+    }
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D2F4.inc", void sub_0805D2F4())
+void sub_0805D2F4(Manager2E* this) {
+    if (gUnk_03003DC0.unk_0xc == 0) {
+        if (this->field_0x38 != SFX_NONE) {
+            SoundReq(this->field_0x38);
+        } else {
+            SoundReq(SFX_SECRET);
+        }
+        if (this->field_0x3c != 0) {
+            SetFlag(this->field_0x3c);
+        }
+        DeleteThisEntity();
+    }
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D32C.inc", void sub_0805D32C())
+void sub_0805D32C(Manager2E* this) {
+    gUnk_08108D50[this->manager.action](this);
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D344.inc", void sub_0805D344())
+void sub_0805D344(Manager2E* this) {
+    CheckFlags(this->field_0x3e);
+    this->manager.action = 1;
+    if (this->manager.unk_0e == 0) {
+        this->manager.unk_0e = 0x1e;
+    }
+    this->manager.unk_0f = this->manager.unk_0e;
+    sub_0805E3A0(this, 6);
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D36C.inc", void sub_0805D36C())
+void sub_0805D36C(Manager2E* this) {
+    if (CheckFlags(this->field_0x3e) != 0) {
+        this->manager.action = 2;
+        this->manager.unk_0e = this->manager.unk_0f;
+    }
+}
 
-ASM_FUNC("asm/non_matching/manager2E/sub_0805D384.inc", void sub_0805D384())
+void sub_0805D384(Manager2E* this) {
+    if (this->manager.unk_0e != 0) {
+        if (--this->manager.unk_0e == 0) {
+            Sound sound;
+            if (this->field_0x38 != SFX_NONE) {
+                sound = this->field_0x38;
+            } else {
+                sound = SFX_SECRET;
+            }
+            SoundReq(sound);
+            SetFlag(this->field_0x3c);
+        }
+    } else {
+        if (CheckFlags(this->field_0x3e) == 0) {
+            this->manager.action = 1;
+            ClearFlag(this->field_0x3c);
+        }
+    }
+}
