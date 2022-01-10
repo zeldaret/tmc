54236cacb58125f54af87734d0c72355d86a0c5c
Tal Hayon <talhayon1@gmail.com>
2022-01-10 06:24:41 +0200

    Review fixes

--- data/const/enemy/bombarossa.s
+++ data/const/enemy/bombarossa.s
@@ -3,14 +3,3 @@
 
 	.section .rodata
     .align 2
-
-gUnk_080CEB38:: @ 080CEB38
-	.4byte sub_080333D4
-	.4byte sub_08033448
-	.4byte sub_08001324
-	.4byte sub_0804A7D4
-	.4byte sub_08001242
-	.4byte nullsub_158
-
-gUnk_080CEB50:: @ 080CEB50
-	.incbin "bombarossa/gUnk_080CEB50.bin"
--- src/enemy/bombarossa.c
+++ src/enemy/bombarossa.c
@@ -9,20 +9,20 @@ typedef struct {
     Entity base;
     u8 filler[0xe];
     u16 unk_0x76;
-} BombarossEntity;
+} BombarossaEntity;
 
-extern void (*const gUnk_080CEB38[])(Entity*);
-extern s8 gUnk_080CEB50[];
+void (*const gUnk_080CEB38[])(Entity*);
+const s8 gUnk_080CEB50[];
 
 extern void sub_080A2CC0(Entity*, void*, void*);
 
-void sub_0803350C(BombarossEntity* this);
+void sub_0803350C(BombarossaEntity* this);
 
 void Bombarossa(Entity* this) {
     gUnk_080CEB38[GetNextFunction(this)](this);
 }
 
-void sub_080333D4(BombarossEntity* this) {
+void sub_080333D4(BombarossaEntity* this) {
     if (super->action == 0) {
         super->action = 1;
         super->actionDelay = Random() & 0xf;
@@ -41,31 +41,30 @@ void sub_080333D4(BombarossEntity* this) {
     }
 }
 
-void sub_08033448(BombarossEntity* this) {
+void sub_08033448(BombarossaEntity* this) {
+    Entity* ent;
     switch (super->bitfield & 0x7f) {
-        default: {
-            Entity* ent = CreateObject(OBJECT_20, 0, 0);
+        default:
+            ent = CreateObject(OBJECT_20, 0, 0);
             if (ent) {
                 CopyPosition(super, ent);
             }
             DeleteThisEntity();
-        }
         case 1:
         case 15:
         case 19:
         case 27:
         case 29:
-        case 30: {
+        case 30:
             sub_0804AA30(super, gUnk_080CEB38);
             break;
-        }
     }
 }
 
 void nullsub_158() {
 }
 
-void sub_0803350C(BombarossEntity* this) {
+void sub_0803350C(BombarossaEntity* this) {
     if ((super->direction & 0x80) == 0) {
         LinearMoveUpdate(super);
     }
@@ -74,3 +73,17 @@ void sub_0803350C(BombarossEntity* this) {
         sub_080A2CC0(super, &super->child, &this->unk_0x76);
     }
 }
+
+void (*const gUnk_080CEB38[])(Entity*) = {
+	(EntityActionPtr) sub_080333D4,
+	(EntityActionPtr) sub_08033448,
+	sub_08001324,
+	sub_0804A7D4,
+	sub_08001242,
+	nullsub_158,
+};
+
+const s8 gUnk_080CEB50[] = {
+    0, -1, -2, -3, -4, -3, -2, -1,
+    -18, -19, -20, -21, -22, -21, -20, -19,
+};
