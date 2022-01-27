44449601b5ba70c5833fbfe9119162f3c4f70b34
Tal Hayon <talhayon1@gmail.com>
2022-01-27 23:31:39 +0200

    Decompile eu variants

--- src/enemy/bombPeahat.c
+++ src/enemy/bombPeahat.c
@@ -188,9 +188,6 @@ void sub_0802AA40(Entity* this) {
     }
 }
 
-#ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_0802AAC0.inc", void sub_0802AAC0(Entity* this))
-#else
 void sub_0802AAC0(Entity* this) {
     sub_0802AC40(this);
     if (--this->field_0xf == 0) {
@@ -204,24 +201,52 @@ void sub_0802AAC0(Entity* this) {
         } else if (ent->next == NULL) {
             this->field_0x80.HALF.HI = 0;
             this->child = NULL;
+#ifdef EU
+        } else if (ent->actionDelay == 0) {
+            if (sub_080002E0(COORD_TO_TILE(this), 1) == 0) {
+                if (EntityInRectRadius(this, &gPlayerEntity, 0x10, 0x10) && ent->field_0xf <= 0x50) {
+                    this->field_0x80.HALF.HI = 0;
+                }
+            } else if (ent->field_0xf <= 0x13) {
+                this->field_0x80.HALF.HI = 0;
+            }
+        }
+#else
         } else if (ent->actionDelay == 0 && ent->field_0xf < 0x51) {
             this->field_0x80.HALF.HI = 0;
         }
+#endif
     } else {
         this->action = 4;
         this->actionDelay = 0xc0;
         this->field_0xf = 4;
         this->field_0x80.HALF.LO ^= 1;
+#ifndef EU
         this->field_0x78.HALF.LO = 0;
         this->field_0x78.HALF.HI = 4;
         this->direction = 0xff;
+#endif
         InitializeAnimation(this, 0);
     }
 }
-#endif
 
 #ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_0802AB40.inc", void sub_0802AB40(Entity* this));
+void sub_0802AB40(Entity* this) {
+    sub_0802AC40(this);
+    if (--this->actionDelay == 0) {
+        if (this->field_0x7a.HALF.LO <= 4) {
+            this->action = 5;
+            this->speed = 0;
+            InitializeAnimation(this, this->type + 1);
+        } else {
+            this->actionDelay = 0xc0;
+            this->field_0xf = 0x4;
+            this->field_0x80.HALF.LO ^= 1;
+        }
+    } else if ((--this->field_0xf & 0xff) == 0) {
+        sub_0802ACDC(this, 4);
+    }
+}
 #else
 void sub_0802AB40(Entity* this) {
     sub_0802AC40(this);
@@ -275,9 +300,6 @@ void sub_0802AC08(Entity* this) {
     GetNextFrame(this);
 }
 
-#ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_0802AC40.inc", void sub_0802AC40(Entity* this))
-#else
 void sub_0802AC40(Entity* this) {
     GetNextFrame(this);
     LinearMoveUpdate(this);
@@ -285,6 +307,7 @@ void sub_0802AC40(Entity* this) {
         if (sub_0802B234(this) == 0) {
             this->field_0x7a.HALF.LO = 0;
             this->spritePriority.b1 = 0;
+#ifndef EU
         } else {
             if (this->z.HALF.HI == 0) {
                 if (this->spritePriority.b1 != 1) {
@@ -295,19 +318,23 @@ void sub_0802AC40(Entity* this) {
                     this->spritePriority.b1 = 3;
                 }
             }
+#endif
         }
     } else {
         if (sub_0802B234(this)) {
             this->field_0x7a.HALF.LO = 1;
+#ifndef EU
             if (this->z.HALF.HI == 0) {
                 this->spritePriority.b1 = 1;
             } else {
                 this->spritePriority.b1 = 3;
             }
+#else
+            this->spritePriority.b1 = 3;
+#endif
         }
     }
 }
-#endif
 
 void sub_0802ACDC(Entity* this, u32 param_2) {
     u32 x;
@@ -356,10 +383,6 @@ void sub_0802AD54(Entity* this) {
     }
 }
 
-#ifdef EU
-// TODO regalloc in EU version
-ASM_FUNC("asm/non_matching/eu/sub_0802ADDC.inc", void sub_0802ADDC(Entity* this))
-#else
 void sub_0802ADDC(Entity* this) {
     Entity* ent = CreateEnemy(BOMB_PEAHAT, this->type + 2);
     if (ent != NULL) {
@@ -367,16 +390,15 @@ void sub_0802ADDC(Entity* this) {
         ent->parent = this;
         this->child = ent;
         CopyPosition(this, ent);
-        /*#ifdef EU
-                this->z.HALF.HI += 8;
-        #endif*/
+#ifdef EU
+        ent->z.HALF.HI += 8;
+#endif
         this->field_0x80.HALF.HI = 1;
         if (this->type == 0) {
             this->field_0x7a.HALF.LO++;
         }
     }
 }
-#endif
 
 void sub_0802AE24(Entity* this) {
     this->action = 1;
@@ -392,7 +414,30 @@ void sub_0802AE24(Entity* this) {
 }
 
 #ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_0802AE68.inc", void sub_0802AE68(Entity* this))
+void sub_0802AE68(Entity* this) {
+    Entity* ent = this->parent;
+    if (ent == 0) {
+        this->action = 3;
+        this->spriteSettings.draw = 1;
+        this->field_0x80.HALF.LO = 1;
+    }
+
+    if (ent->field_0x80.HALF.HI) {
+        CopyPosition(ent, this);
+        this->z.HALF.HI += 8;
+        this->spriteSettings.draw = 0;
+    } else {
+        this->action = 3;
+        this->field_0x80.HALF.LO = 1;
+        this->spriteSettings.draw = 1;
+        if (sub_0802B234(this)) {
+            this->spritePriority.b1 = 3;
+        } else {
+            this->spritePriority.b1 = 0;
+        }
+        GetNextFrame(this);
+    }
+}
 #else
 void sub_0802AE68(Entity* this) {
     Entity* ent = sub_0802B250(this);
@@ -569,7 +614,34 @@ void sub_0802B1A0(Entity* this) {
 }
 
 #ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_0802B1BC.inc", void sub_0802B1BC(Entity* this))
+void sub_0802B1BC(Entity* this) {
+    Entity* ent;
+
+    if (this->actionDelay) {
+        this->actionDelay--;
+    }
+
+    ent = this->parent;
+    if (ent == 0) {
+        this->action = 2;
+        this->spriteSettings.draw = 1;
+    } 
+
+    if (ent->field_0x80.HALF.HI) {
+        CopyPosition(ent, this);
+        this->z.HALF.HI += 8;
+    } else {
+        this->action = 2;
+        this->spriteSettings.draw = 1;
+        if (sub_0802B234(this)) {
+            this->spritePriority.b1 = 3;
+        } else {
+            this->spritePriority.b1 = 0;
+        }
+
+        GetNextFrame(this);
+    }
+}
 #else
 void sub_0802B1BC(Entity* this) {
     Entity* ent;
--- src/manager/manager4.c
+++ src/manager/manager4.c
@@ -91,9 +91,6 @@ u32 sub_0805795C(Manager* this, DiggingCaveEntrance* entr) {
 
 extern void sub_08080930(u32);
 
-#ifdef EU
-ASM_FUNC("asm/non_matching/eu/sub_08057A18.inc", void sub_08057A18(Manager* this, DiggingCaveEntrance* entr))
-#else
 void sub_08057A18(Manager* this, DiggingCaveEntrance* entr) {
     u16 tmp;
     SetInitializationPriority();
@@ -106,6 +103,8 @@ void sub_08057A18(Manager* this, DiggingCaveEntrance* entr) {
     gUnk_03004030.unk_00 = entr;
     gUnk_03004030.unk_04 = gPlayerEntity.x.HALF.HI - gRoomControls.origin_x - ((entr->unk_00 & 0x3F) * 16);
     gUnk_03004030.unk_06 = gPlayerEntity.y.HALF.HI - gRoomControls.origin_y - ((entr->unk_00 & 0xFC0) >> 2);
+
+    #ifndef EU
     tmp = gUnk_03004030.unk_08;
     if (!tmp) {
         if ((entr->target_room | 0x80) != gUnk_03004030.unk_0b) {
@@ -113,10 +112,11 @@ void sub_08057A18(Manager* this, DiggingCaveEntrance* entr) {
         }
         gUnk_03004030.unk_0b = entr->target_room | 0x80;
     }
+    #endif
+    
     sub_08080930(entr->unk_03);
     DeleteManager(this);
 }
-#endif
 
 DiggingCaveEntrance* sub_08057AA8(DiggingCaveEntrance* entr, int room) {
     for (; entr->unk_00 != 0xFFFF; entr++) {
