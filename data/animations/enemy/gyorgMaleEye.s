192419c7addfe4f91aefb8fdcfd39a03e5ac87bb
octorock <79596758+octorock@users.noreply.github.com>
2022-02-05 13:07:26 +0100

    Decompile Enemy62

---
+++ data/animations/enemy/gyorgMaleEye.s
@@ -0,0 +1,264 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+    .align 2
+
+gSpriteAnimations_GyorgMaleEye_0:: @ 080D2368
+	.include "animations/gSpriteAnimations_GyorgMaleEye_0.s"
+
+gSpriteAnimations_GyorgMaleEye_1:: @ 080D2379
+	.include "animations/gSpriteAnimations_GyorgMaleEye_1.s"
+
+gSpriteAnimations_GyorgMaleEye_2:: @ 080D238A
+	.include "animations/gSpriteAnimations_GyorgMaleEye_2.s"
+
+gSpriteAnimations_GyorgMaleEye_3:: @ 080D239B
+	.include "animations/gSpriteAnimations_GyorgMaleEye_3.s"
+
+gSpriteAnimations_GyorgMaleEye_4:: @ 080D23AC
+	.include "animations/gSpriteAnimations_GyorgMaleEye_4.s"
+
+gSpriteAnimations_GyorgMaleEye_5:: @ 080D23BD
+	.include "animations/gSpriteAnimations_GyorgMaleEye_5.s"
+
+gSpriteAnimations_GyorgMaleEye_6:: @ 080D23CE
+	.include "animations/gSpriteAnimations_GyorgMaleEye_6.s"
+
+gSpriteAnimations_GyorgMaleEye_7:: @ 080D23DF
+	.include "animations/gSpriteAnimations_GyorgMaleEye_7.s"
+
+gSpriteAnimations_GyorgMaleEye_8:: @ 080D23F0
+	.include "animations/gSpriteAnimations_GyorgMaleEye_8.s"
+
+gSpriteAnimations_GyorgMaleEye_9:: @ 080D2401
+	.include "animations/gSpriteAnimations_GyorgMaleEye_9.s"
+
+gSpriteAnimations_GyorgMaleEye_10:: @ 080D2412
+	.include "animations/gSpriteAnimations_GyorgMaleEye_10.s"
+
+gSpriteAnimations_GyorgMaleEye_11:: @ 080D2423
+	.include "animations/gSpriteAnimations_GyorgMaleEye_11.s"
+
+gSpriteAnimations_GyorgMaleEye_12:: @ 080D2434
+	.include "animations/gSpriteAnimations_GyorgMaleEye_12.s"
+
+gSpriteAnimations_GyorgMaleEye_13:: @ 080D2445
+	.include "animations/gSpriteAnimations_GyorgMaleEye_13.s"
+
+gSpriteAnimations_GyorgMaleEye_14:: @ 080D2456
+	.include "animations/gSpriteAnimations_GyorgMaleEye_14.s"
+
+gSpriteAnimations_GyorgMaleEye_15:: @ 080D2467
+	.include "animations/gSpriteAnimations_GyorgMaleEye_15.s"
+
+gSpriteAnimations_GyorgMaleEye_16:: @ 080D2478
+	.include "animations/gSpriteAnimations_GyorgMaleEye_16.s"
+
+gSpriteAnimations_GyorgMaleEye_17:: @ 080D2489
+	.include "animations/gSpriteAnimations_GyorgMaleEye_17.s"
+
+gSpriteAnimations_GyorgMaleEye_18:: @ 080D249A
+	.include "animations/gSpriteAnimations_GyorgMaleEye_18.s"
+
+gSpriteAnimations_GyorgMaleEye_19:: @ 080D24AB
+	.include "animations/gSpriteAnimations_GyorgMaleEye_19.s"
+
+gSpriteAnimations_GyorgMaleEye_20:: @ 080D24BC
+	.include "animations/gSpriteAnimations_GyorgMaleEye_20.s"
+
+gSpriteAnimations_GyorgMaleEye_21:: @ 080D24CD
+	.include "animations/gSpriteAnimations_GyorgMaleEye_21.s"
+
+gSpriteAnimations_GyorgMaleEye_22:: @ 080D24DE
+	.include "animations/gSpriteAnimations_GyorgMaleEye_22.s"
+
+gSpriteAnimations_GyorgMaleEye_23:: @ 080D24EF
+	.include "animations/gSpriteAnimations_GyorgMaleEye_23.s"
+
+gSpriteAnimations_GyorgMaleEye_24:: @ 080D2500
+	.include "animations/gSpriteAnimations_GyorgMaleEye_24.s"
+
+gSpriteAnimations_GyorgMaleEye_25:: @ 080D2511
+	.include "animations/gSpriteAnimations_GyorgMaleEye_25.s"
+
+gSpriteAnimations_GyorgMaleEye_26:: @ 080D2522
+	.include "animations/gSpriteAnimations_GyorgMaleEye_26.s"
+
+gSpriteAnimations_GyorgMaleEye_27:: @ 080D2533
+	.include "animations/gSpriteAnimations_GyorgMaleEye_27.s"
+
+gSpriteAnimations_GyorgMaleEye_28:: @ 080D2544
+	.include "animations/gSpriteAnimations_GyorgMaleEye_28.s"
+
+gSpriteAnimations_GyorgMaleEye_29:: @ 080D2555
+	.include "animations/gSpriteAnimations_GyorgMaleEye_29.s"
+
+gSpriteAnimations_GyorgMaleEye_30:: @ 080D2566
+	.include "animations/gSpriteAnimations_GyorgMaleEye_30.s"
+
+gSpriteAnimations_GyorgMaleEye_31:: @ 080D2577
+	.include "animations/gSpriteAnimations_GyorgMaleEye_31.s"
+
+gSpriteAnimations_GyorgMaleEye_32:: @ 080D2588
+	.include "animations/gSpriteAnimations_GyorgMaleEye_32.s"
+
+gSpriteAnimations_GyorgMaleEye_33:: @ 080D2599
+	.include "animations/gSpriteAnimations_GyorgMaleEye_33.s"
+
+gSpriteAnimations_GyorgMaleEye_34:: @ 080D25AA
+	.include "animations/gSpriteAnimations_GyorgMaleEye_34.s"
+
+gSpriteAnimations_GyorgMaleEye_35:: @ 080D25BB
+	.include "animations/gSpriteAnimations_GyorgMaleEye_35.s"
+
+gSpriteAnimations_GyorgMaleEye_36:: @ 080D25CC
+	.include "animations/gSpriteAnimations_GyorgMaleEye_36.s"
+
+gSpriteAnimations_GyorgMaleEye_37:: @ 080D25DD
+	.include "animations/gSpriteAnimations_GyorgMaleEye_37.s"
+
+gSpriteAnimations_GyorgMaleEye_38:: @ 080D25EE
+	.include "animations/gSpriteAnimations_GyorgMaleEye_38.s"
+
+gSpriteAnimations_GyorgMaleEye_39:: @ 080D25FF
+	.include "animations/gSpriteAnimations_GyorgMaleEye_39.s"
+
+gSpriteAnimations_GyorgMaleEye_40:: @ 080D2610
+	.include "animations/gSpriteAnimations_GyorgMaleEye_40.s"
+
+gSpriteAnimations_GyorgMaleEye_41:: @ 080D2621
+	.include "animations/gSpriteAnimations_GyorgMaleEye_41.s"
+
+gSpriteAnimations_GyorgMaleEye_42:: @ 080D2632
+	.include "animations/gSpriteAnimations_GyorgMaleEye_42.s"
+
+gSpriteAnimations_GyorgMaleEye_43:: @ 080D2643
+	.include "animations/gSpriteAnimations_GyorgMaleEye_43.s"
+
+gSpriteAnimations_GyorgMaleEye_44:: @ 080D2654
+	.include "animations/gSpriteAnimations_GyorgMaleEye_44.s"
+
+gSpriteAnimations_GyorgMaleEye_45:: @ 080D2665
+	.include "animations/gSpriteAnimations_GyorgMaleEye_45.s"
+
+gSpriteAnimations_GyorgMaleEye_46:: @ 080D2676
+	.include "animations/gSpriteAnimations_GyorgMaleEye_46.s"
+
+gSpriteAnimations_GyorgMaleEye_47:: @ 080D2687
+	.include "animations/gSpriteAnimations_GyorgMaleEye_47.s"
+
+gSpriteAnimations_GyorgMaleEye_48:: @ 080D2698
+	.include "animations/gSpriteAnimations_GyorgMaleEye_48.s"
+
+gSpriteAnimations_GyorgMaleEye_49:: @ 080D26A9
+	.include "animations/gSpriteAnimations_GyorgMaleEye_49.s"
+
+gSpriteAnimations_GyorgMaleEye_50:: @ 080D26BA
+	.include "animations/gSpriteAnimations_GyorgMaleEye_50.s"
+
+gSpriteAnimations_GyorgMaleEye_51:: @ 080D26CB
+	.include "animations/gSpriteAnimations_GyorgMaleEye_51.s"
+
+gSpriteAnimations_GyorgMaleEye_52:: @ 080D26DC
+	.include "animations/gSpriteAnimations_GyorgMaleEye_52.s"
+
+gSpriteAnimations_GyorgMaleEye_53:: @ 080D26ED
+	.include "animations/gSpriteAnimations_GyorgMaleEye_53.s"
+
+gSpriteAnimations_GyorgMaleEye_54:: @ 080D26FE
+	.include "animations/gSpriteAnimations_GyorgMaleEye_54.s"
+
+gSpriteAnimations_GyorgMaleEye_55:: @ 080D270F
+	.include "animations/gSpriteAnimations_GyorgMaleEye_55.s"
+
+gSpriteAnimations_GyorgMaleEye_56:: @ 080D2720
+	.include "animations/gSpriteAnimations_GyorgMaleEye_56.s"
+
+gSpriteAnimations_GyorgMaleEye_57:: @ 080D2731
+	.include "animations/gSpriteAnimations_GyorgMaleEye_57.s"
+
+gSpriteAnimations_GyorgMaleEye_58:: @ 080D2742
+	.include "animations/gSpriteAnimations_GyorgMaleEye_58.s"
+
+gSpriteAnimations_GyorgMaleEye_59:: @ 080D2753
+	.include "animations/gSpriteAnimations_GyorgMaleEye_59.s"
+
+gSpriteAnimations_GyorgMaleEye_60:: @ 080D2764
+	.include "animations/gSpriteAnimations_GyorgMaleEye_60.s"
+
+gSpriteAnimations_GyorgMaleEye_61:: @ 080D2775
+	.include "animations/gSpriteAnimations_GyorgMaleEye_61.s"
+
+gSpriteAnimations_GyorgMaleEye_62:: @ 080D2786
+	.include "animations/gSpriteAnimations_GyorgMaleEye_62.s"
+
+gSpriteAnimations_GyorgMaleEye_63:: @ 080D2797
+	.include "animations/gSpriteAnimations_GyorgMaleEye_63.s"
+
+gSpriteAnimations_GyorgMaleEye:: @ 080D27A8
+	.4byte gSpriteAnimations_GyorgMaleEye_0
+	.4byte gSpriteAnimations_GyorgMaleEye_1
+	.4byte gSpriteAnimations_GyorgMaleEye_2
+	.4byte gSpriteAnimations_GyorgMaleEye_3
+	.4byte gSpriteAnimations_GyorgMaleEye_4
+	.4byte gSpriteAnimations_GyorgMaleEye_5
+	.4byte gSpriteAnimations_GyorgMaleEye_6
+	.4byte gSpriteAnimations_GyorgMaleEye_7
+	.4byte gSpriteAnimations_GyorgMaleEye_8
+	.4byte gSpriteAnimations_GyorgMaleEye_9
+	.4byte gSpriteAnimations_GyorgMaleEye_10
+	.4byte gSpriteAnimations_GyorgMaleEye_11
+	.4byte gSpriteAnimations_GyorgMaleEye_12
+	.4byte gSpriteAnimations_GyorgMaleEye_13
+	.4byte gSpriteAnimations_GyorgMaleEye_14
+	.4byte gSpriteAnimations_GyorgMaleEye_15
+	.4byte gSpriteAnimations_GyorgMaleEye_16
+	.4byte gSpriteAnimations_GyorgMaleEye_17
+	.4byte gSpriteAnimations_GyorgMaleEye_18
+	.4byte gSpriteAnimations_GyorgMaleEye_19
+	.4byte gSpriteAnimations_GyorgMaleEye_20
+	.4byte gSpriteAnimations_GyorgMaleEye_21
+	.4byte gSpriteAnimations_GyorgMaleEye_22
+	.4byte gSpriteAnimations_GyorgMaleEye_23
+	.4byte gSpriteAnimations_GyorgMaleEye_24
+	.4byte gSpriteAnimations_GyorgMaleEye_25
+	.4byte gSpriteAnimations_GyorgMaleEye_26
+	.4byte gSpriteAnimations_GyorgMaleEye_27
+	.4byte gSpriteAnimations_GyorgMaleEye_28
+	.4byte gSpriteAnimations_GyorgMaleEye_29
+	.4byte gSpriteAnimations_GyorgMaleEye_30
+	.4byte gSpriteAnimations_GyorgMaleEye_31
+	.4byte gSpriteAnimations_GyorgMaleEye_32
+	.4byte gSpriteAnimations_GyorgMaleEye_33
+	.4byte gSpriteAnimations_GyorgMaleEye_34
+	.4byte gSpriteAnimations_GyorgMaleEye_35
+	.4byte gSpriteAnimations_GyorgMaleEye_36
+	.4byte gSpriteAnimations_GyorgMaleEye_37
+	.4byte gSpriteAnimations_GyorgMaleEye_38
+	.4byte gSpriteAnimations_GyorgMaleEye_39
+	.4byte gSpriteAnimations_GyorgMaleEye_40
+	.4byte gSpriteAnimations_GyorgMaleEye_41
+	.4byte gSpriteAnimations_GyorgMaleEye_42
+	.4byte gSpriteAnimations_GyorgMaleEye_43
+	.4byte gSpriteAnimations_GyorgMaleEye_44
+	.4byte gSpriteAnimations_GyorgMaleEye_45
+	.4byte gSpriteAnimations_GyorgMaleEye_46
+	.4byte gSpriteAnimations_GyorgMaleEye_47
+	.4byte gSpriteAnimations_GyorgMaleEye_48
+	.4byte gSpriteAnimations_GyorgMaleEye_49
+	.4byte gSpriteAnimations_GyorgMaleEye_50
+	.4byte gSpriteAnimations_GyorgMaleEye_51
+	.4byte gSpriteAnimations_GyorgMaleEye_52
+	.4byte gSpriteAnimations_GyorgMaleEye_53
+	.4byte gSpriteAnimations_GyorgMaleEye_54
+	.4byte gSpriteAnimations_GyorgMaleEye_55
+	.4byte gSpriteAnimations_GyorgMaleEye_56
+	.4byte gSpriteAnimations_GyorgMaleEye_57
+	.4byte gSpriteAnimations_GyorgMaleEye_58
+	.4byte gSpriteAnimations_GyorgMaleEye_59
+	.4byte gSpriteAnimations_GyorgMaleEye_60
+	.4byte gSpriteAnimations_GyorgMaleEye_61
+	.4byte gSpriteAnimations_GyorgMaleEye_62
+	.4byte gSpriteAnimations_GyorgMaleEye_63
+	.4byte 00000000
