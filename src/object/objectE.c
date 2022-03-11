c09f143423955fdca6db1b6bb588c4a11d1d4b67
octorock <79596758+octorock@users.noreply.github.com>
2022-03-11 18:45:07 +0100

    Decompile ObjectE

---
+++ src/object/objectE.c
@@ -0,0 +1,26 @@
+/**
+ * @file objectE.c
+ * @ingroup Objects
+ *
+ * @brief ObjectE object
+ */
+
+#define NENT_DEPRECATED
+#include "global.h"
+#include "object.h"
+#include "functions.h"
+
+void ObjectE(Entity* this) {
+    if (this->action == 0) {
+        this->action = 1;
+        this->actionDelay = 0x14;
+        sub_08004168(this);
+    } else {
+        if (this->actionDelay-- == 0) {
+            sub_0807B7D8(0x35, COORD_TO_TILE(this), this->collisionLayer);
+            CreateFx(this, FX_FALL_DOWN, 0x40);
+            SoundReq(SFX_126);
+            DeleteThisEntity();
+        }
+    }
+}
