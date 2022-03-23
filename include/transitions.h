eb5d9a67264dcf22488a8b66b060517ee2cb7ad0
Henny022p <info@henny022.de>
2022-03-23 22:38:09 +0100

    warp and transition type

--- include/transitions.h
+++ include/transitions.h
@@ -4,10 +4,26 @@
 #include "roomid.h"
 
 typedef enum {
-    WARP_TYPE_0,
-    WARP_TYPE_1,
+    WARP_TYPE_BORDER,
+    WARP_TYPE_AREA,
     WARP_TYPE_END_OF_LIST = -1,
 } WarpType;
+
+typedef enum {
+    TRANSITION_TYPE_NORMAL,
+    TRANSITION_TYPE_INSTANT_MINISH,
+    TRANSITION_TYPE_DROP_IN,
+    TRANSITION_TYPE_INSTANT,
+    TRANSITION_TYPE_STEP_FORWARD,
+    TRANSITION_TYPE_5,
+    TRANSITION_TYPE_DROP_IN_MINISH,
+    TRANSITION_TYPE_STAIR_EXIT,
+    TRANSITION_TYPE_8,
+    TRANSITION_TYPE_9,
+    TRANSITION_TYPE_HOP_IN_FORWARD,
+    TRANSITION_TYPE_HOP_IN,
+    TRANSITION_TYPE_FLY_IN,
+} TransitionType;
 
 typedef struct Transition {
     WarpType warp_type : 8;
@@ -20,8 +36,8 @@ typedef struct Transition {
     u8 area;
     RoomID room : 8;
     u8 height;
-    u8 transition_type;
-    u8 facing_direction;
+    TransitionType transition_type : 8;
+    u8 facing_direction; // 0-4
     u8 unk0;
     u8 unk1;
     u8 unk2;
