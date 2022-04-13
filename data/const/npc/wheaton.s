d7fbc73414f540c23b61d8032a5160fdf73fdcde
Tal Hayon <talhayon1@gmail.com>
2022-04-13 23:06:06 +0300

    Put const data into wheaton

--- data/const/npc/wheaton.s
+++
@@ -1,8 +0,0 @@
-	.include "asm/macros.inc"
-	.include "constants/constants.inc"
-
-	.section .rodata
-    .align 2
-
-gUnk_0810C3C0:: @ 0810C3C0
-	.incbin "wheaton/gUnk_0810C3C0.bin"
--- src/npc/wheaton.c
+++ src/npc/wheaton.c
@@ -1,14 +1,13 @@
 #include "npc.h"
 
-extern Hitbox gUnk_0810C3C0;
-
 void Wheaton(Entity* this) {
+    static const Hitbox gUnk_0810C3C0 = { 0, 4, { 0, 0, 0, 0 }, 6, 12 };
     if (this->action == 0) {
         this->action++;
 
         SetDefaultPriority(this, PRIO_MESSAGE);
         SortEntityAbove(this, this);
-        this->hitbox = &gUnk_0810C3C0;
+        this->hitbox = (Hitbox*)&gUnk_0810C3C0;
         this->spriteOffsetY = 0xfe;
         sub_0807DD64(this);
     }
