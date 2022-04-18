868c950e5091d900b9af557e9b702c3b06a2949e
Tal Hayon <talhayon1@gmail.com>
2022-04-18 14:11:34 +0300

    Put const data in pinwheel

--- src/object/pinwheel.c
+++ src/object/pinwheel.c
@@ -3,11 +3,21 @@
 #include "flags.h"
 #include "object.h"
 
+static const u16 gUnk_08125050[] = {
+    KUMOUE_02_AWASE_01, KUMOUE_02_AWASE_02, KUMOUE_02_AWASE_03, KUMOUE_02_AWASE_04, KUMOUE_02_AWASE_05, BEGIN_1,
+};
 extern u32 gUnk_020342F8;
-extern u16 gUnk_08125050[];
-extern void (*gUnk_0812505C[])(Entity*);
+
+void sub_080A23DC(Entity*);
+void sub_080A2420(Entity*);
+void sub_080A243C(Entity*);
 
 void Pinwheel(Entity* this) {
+    static void (*const gUnk_0812505C[])(Entity*) = {
+        sub_080A23DC,
+        sub_080A2420,
+        sub_080A243C,
+    };
     u16 x = this->health;
     if ((x & 0x7f) != 0) {
         if (ReadBit(&gUnk_020342F8, x - 1) == 0) {
